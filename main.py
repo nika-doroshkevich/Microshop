3bf5a205562347152c6536f5715adb397acb7f48
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-07-17 19:31:40 +0300

    no more create all on app start

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ from fastapi import FastAPI
 
 from api_v1 import router as router_v1
 from core.config import settings
-from core.models import Base, db_helper
 from items_views import router as items_router
 from users.views import router as users_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
