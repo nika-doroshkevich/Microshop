f2e1a1a9fa4ace062bacd0c6ecd5bc42871df4da
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-08-02 16:03:50 +0300

    Basic Auth, Token Auth, Cookie Auth

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from .demo_auth.views import router as demo_auth_router
 from .products.views import router as products_router
 
 router = APIRouter()
 router.include_router(router=products_router, prefix="/products")
+router.include_router(router=demo_auth_router)
